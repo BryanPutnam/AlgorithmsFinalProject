b5fa99819b427ae994a23c4109f9421d3aa3372d
Bryan Putnam <bryanputnam25@gmail.com>
2023-04-24 16:05:23 -0700

    revised random number generators

--- Main.java
+++ Main.java
@@ -14,15 +14,12 @@ public class Main {
     static int edges; 
     static int graphType; 
     static int distroType; 
-    
-    static Random skewRand = new Random(); 
-    static Random normalRand = new Random();
 
     public static void main(String args[]) { // Input in this order: Vertices, Edges, Graph Type, Distro Type
         vertices = Integer.parseInt(args[0]); 
         edges = Integer.parseInt(args[1]); 
         graphType = Integer.parseInt(args[2]); // 1=Complete | 2=Cycle  | 3=Random 
-        distroType = Integer.parseInt(args[3]); // 1=Uniform | 2=Skewed | 3=Normal 
+        distroType = Integer.parseInt(args[3]); // 1=Uniform | 2=Skewed | 3=Normal  
 
         getDistro(); 
     }
@@ -50,13 +47,13 @@ public class Main {
     public static void getDistro() { 
         switch(distroType) { 
             case 1: 
-                uniformInteger(vertices); // Returns the random Int
+            System.out.println(uniformDistro()); // Returns the random Int
                 break; 
             case 2: 
-                skewedDouble(vertices, skewRand); // Returns the random Double
+            System.out.println(skewedDistro()); // Returns the random Double
                 break; 
             case 3:
-                normalInteger(vertices, normalRandom); //Returns the random Int
+            System.out.println(normalDistro()); //Returns the random Int
                 break;  
             default: 
                 System.out.println("No such distrobution exists");
@@ -67,32 +64,53 @@ public class Main {
         DISTRIBUTIONS CODE (Uniform, Skewed, Normal)
     */
 
-    public static int uniformInteger(int vertices) { 
+    public static int uniformDistro() { 
         int uniRand = (int)(Math.random() * vertices) + 1;
         return uniRand; 
     }
 
-    public static int skewedDouble(int vertices, Random skewRand) { 
-        double max = (double)(vertices); 
-        double min = 1; 
-        double bias = 5; 
-        double skew = 5; 
-        double range = max - min; 
-        double mid = min + range / 2.0; 
-        double unitGaussian = skewRand.nextGaussian(); 
-        double biasFactor = Math.exp(bias); 
-        double skewNum = mid + (range * (biasFactor/(biasFactor + Math.exp(-unitGaussian/skew))-0.5)); //StackOverflow //WHAT DIS MEAN?????
-        return (int)(skewNum); 
-
+    public static int skewedDistro() {  
+        double skew = 2; 
+        double unscaledSkewNum = Math.pow(Math.random(), skew);
+        double skewNum = unscaledSkewNum * (vertices - 1) + 1; // scale from 1 to vertices
+        return (int)(skewNum);
     }
 
-    public static int normalInteger(int vertices, Random normalRandom) { 
+    public static int normalDistro() {
+
+        double num1 = Math.random(); 
+        double num2 = Math.random(); 
+        double mean = normMean(); 
+        double variance = normVariance(mean); 
+        double stdDev = normStddev(mean, variance); 
+
+        double rand = Math.sqrt(-2.0 * Math.log(num1)) * Math.sin(2.0 * Math.PI * num2);
+        double unscaledNormNum = mean + stdDev * rand; 
+
+        double min1 = mean - (stdDev * 4); 
+        double max1 = mean + (stdDev * 4); 
+        double min2 = 1; 
+        double max2 = vertices; 
+
+        double normNum = ((unscaledNormNum - min1) / (max1 - min1)) * (max2 - min2) + min2; 
+        
+        return (int)(normNum); 
+    }
+
+    public static double normMean() { 
         int sum = IntStream.rangeClosed(1, vertices).sum();
         double mean = (sum / vertices); // mean of range
-        double variance = IntStream.rangeClosed(1, vertices).mapToDouble(i -> Math.pow(i - mean, 2)).average().orElse(Double.NaN); //ChatGPT variance calculation //WHAT DIS MEAN x2
+        return mean; 
+    }
+
+    public static double normVariance(double mean) { 
+        double variance = IntStream.rangeClosed(1, vertices).mapToDouble(i -> Math.pow(i - mean, 2)).average().orElse(Double.NaN); //ChatGPT variance calculation
+        return variance; 
+    }
+
+    public static double normStddev(double mean, double variance) { 
         double stdDev = Math.sqrt(variance); 
-        double num = normalRandom.nextGaussian() * stdDev + mean; 
-        return (int)(num); 
+        return stdDev; 
     }
 
     /*
